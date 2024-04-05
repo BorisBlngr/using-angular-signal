f621699f4916701ed4502c03e51bac3ee7fdd87d
Boris Boulanger <boris.boulanger@vidal.fr>
2024-04-05 16:02:04 +0200

    signal dans le template

--- src/app/vitrine/vitrine.component.ts
+++ src/app/vitrine/vitrine.component.ts
@@ -1,21 +1,28 @@
-import {Component, effect, input} from '@angular/core';
+import {Component, effect, input, signal} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-vitrine',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './vitrine.component.html',
   styleUrl: './vitrine.component.scss'
 })
 export class VitrineComponent {
   aFaireListeSignal = input.required<string[]>();
 
+  secondSignal = signal('');
+
   count = 0;
 
   constructor() {
     effect(() => {
       this.count++;
-      console.log(`appel numéro ${this.count}, nouvelle valeur`, this.aFaireListeSignal());
+      console.log(`appel numéro ${this.count},
+       nouvelle valeur : ${this.aFaireListeSignal()}
+       et ${this.secondSignal}`);
     });
   }
 
